0f79a4d6318c27cf5849ac15744989928f611884
Nico Cobelo <cobelon@gmail.com>
2020-10-07 13:22:22 -0300

    Managed to itirate over csv file

--- import.py
+++ import.py
@@ -6,21 +6,28 @@ import sys
 if len(sys.argv) != 2:
     print("Usage: python houses.py characters.csv")
     sys.exit(1)
-    
+
 # Create database
-open("students.db", "w").close()
-db = SQL("sqlite:///students.db")
+#open("students.db", "w").close()
+#db = SQL("sqlite:///students.db")
 
 # Create tables
-db.execute("INSERT")
-    
+#db.execute("INSERT")
+
 # Open csv file and read into memory
 with open(f"{sys.argv[1]}", 'r') as characters:
 
     # Create DictReader
-    reader = csv.DictReader(characters, delimiter=",")
-    
-    # Iterate over CSV file
-    for row in reader:
-        
-        
+    reader = csv.DictReader(characters)
+
+    # Create new csv file to write to
+    with open("students.csv","w") as students:
+        writer = csv.writer(students)
+        # Write the headers
+        writer.writerow(["first", "middle", "last", "house", "birth"])
+
+        # Iterate over CSV file
+        for row in reader:
+            
+            writer.writerow([row["name"], row["house"], row["birth"]])
+
